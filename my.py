2b8c00aac23d993e6fd403017bc88616d9897236
YasiruNilupul <wickramageyn.22@UOM.lk>
2023-04-03 17:08:43 +0530

    Update my.py

--- my.py
+++ my.py
@@ -1,15 +1,14 @@
-k=0
-num=1
+primes = [2, 3, 5, 7, 11, 13, 17, 19]
+num = 1
+for prime in primes:
+    num *= prime
+
 while True:
-    for i in range(1,21):
-        if num%i!=0:
+    for i in range(1, 21):
+        if num % i != 0:
             break
-        else:
-            k+=1
-        
-    if k==20:
+    else:
         print(num)
         break
-    k=0
-    num+=1
 
+    num += primes[-1]  # Increment by the product of primes
